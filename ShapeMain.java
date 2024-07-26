ecf19d06b7f197d081b502f9e166944ad34d85d3
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-26 22:41:05 +0530

    Add files via upload

---
+++ ShapeMain.java
@@ -0,0 +1,11 @@
+public class ShapeMain {
+
+	public static void main(String[] args) 
+	{
+		Circle obj=new Circle(5);
+		obj.AreaOfShape();
+		Rectangle rec=new Rectangle(12,20);
+		rec.AreaOfShape();
+	}
+
+}
