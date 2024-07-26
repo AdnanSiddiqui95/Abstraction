82b37984dab090fd3b802f267a0e856dbe1062d9
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-26 22:18:34 +0530

    Add files via upload

---
+++ BB.java
@@ -0,0 +1,16 @@
+public class BB extends AA
+{
+		public void method1()
+		{
+			System.out.println("adnan");
+		}
+		public void method2()
+		{
+			System.out.println("Salman");
+		}
+		public void method3()
+		{
+			System.out.println("Rehan");
+		}
+
+}
