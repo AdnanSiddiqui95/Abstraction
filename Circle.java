1b25201250f8b750ce1bc7abb858b99656d6ac6b
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-26 22:40:33 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,14 @@
+
+public class Circle extends Shape
+{
+	private int radius;
+	public Circle(int radius) 
+	{
+		this.radius=radius;
+	}
+	public void AreaOfShape()
+	{
+		double area=3.14*(radius*radius);
+		System.out.println("Area of Circle : "+area);
+	}
+}
